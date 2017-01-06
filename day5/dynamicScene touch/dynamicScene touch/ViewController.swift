4a193088d3cfb5427d4ec434e34a354063a37694
Tacademy-Mac <tacademy-mac@Tacademy-Macui-MacBook-Pro.local>
2017-01-06 11:54:41 +0900

    added dynamicScene touch

--- day5/dynamicScene touch/dynamicScene touch/ViewController.swift
+++ day5/dynamicScene touch/dynamicScene touch/ViewController.swift
@@ -9,17 +9,48 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var ballImage: UIImageView!
+    
+    var holdingImage = false
+    var distance = CGPoint(x: 0, y: 0)
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touch = touches.first!
+        let point = touch.location(in: touch.view)
+        
+        if ballImage.frame.contains(point){
+            holdingImage = true
+            ballImage.transform = ballImage.transform.scaledBy(x: 1.1, y: 1.1)
+            
+            distance.x = ballImage.center.x - point.x
+            distance.y = ballImage.center.y - point.y
+        }
+        else {
+            holdingImage = false
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if holdingImage {
+            let touch = touches.first!
+            let point = touch.location(in: touch.view)
+            
+            let pointOfBallCenter = CGPoint(x: point.x + self.distance.x, y: point.y + self.distance.y)
+            
+            // print(pointOfBallCenter)
+            
+            ballImage.center = pointOfBallCenter
+        }
     }
-
-
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if holdingImage {
+            ballImage.transform = CGAffineTransform.identity
+            holdingImage = false
+        }
+    }
+    
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        print("cancel")
+    }
 }
 
